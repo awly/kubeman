f9eaf2a806919f8e70e6697bc54cfb1a8568cd1c
Andrew Lytvynov <lytvynov.a.v@gmail.com>
2015-06-01 15:45:57 -0700

    ui: add more columns to pods tab
    Container status, container start time and pod restart count.

--- ui/pods.go
+++ ui/pods.go
@@ -3,7 +3,9 @@ package ui
 import (
 	"log"
 	"sort"
+	"strconv"
 	"sync"
+	"time"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/watch"
@@ -62,12 +64,18 @@ func (pl *podsTab) toRows() []*termui.Row {
 	lstatus := header("status")
 	lhost := header("host")
 	lcont := header("container")
+	lcontStatus := header("cont status")
+	lcontStarted := header("started at")
+	lrestarts := header("restarts")
 
 	rows = append(rows, termui.NewRow(
-		termui.NewCol(4, 0, lname),
-		termui.NewCol(2, 0, lstatus),
-		termui.NewCol(3, 0, lhost),
+		termui.NewCol(3, 0, lname),
+		termui.NewCol(1, 0, lstatus),
+		termui.NewCol(2, 0, lhost),
 		termui.NewCol(3, 0, lcont),
+		termui.NewCol(1, 0, lcontStatus),
+		termui.NewCol(1, 0, lcontStarted),
+		termui.NewCol(1, 0, lrestarts),
 	))
 	for _, p := range pl.pods {
 		rows = append(rows, p.toRows()...)
@@ -93,18 +101,37 @@ func (pr pod) toRows() []*termui.Row {
 	lhost := label(pr.p.Spec.Host)
 
 	rows := make([]*termui.Row, 0, len(pr.p.Spec.Containers))
-	for i, c := range pr.p.Spec.Containers {
+	for i, c := range pr.p.Status.ContainerStatuses {
 		if i > 0 {
 			lname = label("")
 			lstatus = label("")
 			lhost = label("")
 		}
 		lcont := label(c.Image)
+		lcontStatus := label("")
+		lcontStarted := label("")
+		switch {
+		case c.State.Running != nil:
+			lcontStatus.Text = "Running"
+			lcontStatus.TextFgColor = termui.ColorGreen
+			lcontStarted.Text = c.State.Running.StartedAt.Format(time.Stamp)
+		case c.State.Termination != nil:
+			lcontStatus.Text = "Terminated"
+			lcontStatus.TextFgColor = termui.ColorRed
+			lcontStarted.Text = c.State.Termination.StartedAt.String()
+		default:
+			lcontStatus.Text = "Waiting"
+			lcontStatus.TextFgColor = termui.ColorYellow
+		}
+		lrestarts := label(strconv.Itoa(c.RestartCount))
 		rows = append(rows, termui.NewRow(
-			termui.NewCol(4, 0, lname),
-			termui.NewCol(2, 0, lstatus),
-			termui.NewCol(3, 0, lhost),
+			termui.NewCol(3, 0, lname),
+			termui.NewCol(1, 0, lstatus),
+			termui.NewCol(2, 0, lhost),
 			termui.NewCol(3, 0, lcont),
+			termui.NewCol(1, 0, lcontStatus),
+			termui.NewCol(1, 0, lcontStarted),
+			termui.NewCol(1, 0, lrestarts),
 		))
 	}
 	return rows
